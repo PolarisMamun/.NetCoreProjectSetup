bf772af8c2d8830b569ebf0687fcbff5b6fcd3b4
Munsi Abdul Al Mamun <31066908+PolarisMamun@users.noreply.github.com>
2023-12-04 18:13:19 +0800

    Create Scaffolding Controllers and Actions.cs
    Scaffolding Controllers
    and Key terms

---
+++ Scaffolding Controllers and Actions.cs
@@ -0,0 +1,21 @@
+//To Scaffolding in controller folder click on right button and add a controller select API controller with action,using entity framework then chose model class,dbcontext class ,and controller name
+
+
+//In the context of .NET and web development, "scaffolding" generally refers to the automatic generation of code and files for common tasks in building a web application, particularly in the context of web APIs. 
+Scaffolding is a code generation technique that helps developers quickly create the basic structure and functionality of certain components, such as controllers, views, models, and other artifacts.
+
+
+  Key Terms and Definitions
+Our scaffolded code features some keywords that are defined below for full understanding.
+
+Task - A task in C# is used to implement Task-based Asynchronous Programming. The Task object is typically executed asynchronously 
+  on a thread pool thread rather than synchronously on the main thread of the application.
+
+async - Signals to the compiler that this method contains an await statement; it contains asynchronous operations.
+
+await - The await keyword provides a non-blocking way to start a task, then continue execution when that task completes.
+
+ActionResult - An action is capable of returning a specific data type (see WeatherForecastController action).  When multiple return types are possible, 
+  it's common to return ActionResult, IActionResult or ActionResult<T>, where T represents the data type to be returned.
+
+  
